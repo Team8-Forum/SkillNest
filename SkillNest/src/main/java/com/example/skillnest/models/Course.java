92b4aa12d854417252777dd69d1a95e06b0b86f5
Toma Mishev <47216444+TomaMishev@users.noreply.github.com>
2023-11-29 16:58:46 +0200

    Updating DB

--- SkillNest/src/main/java/com/example/skillnest/models/Course.java
+++ SkillNest/src/main/java/com/example/skillnest/models/Course.java
@@ -1,4 +1,28 @@
 package com.example.skillnest.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "courses")
+
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "topic")
+    private String topic;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "starting_date")
+    private LocalDate startingDate;
 }
