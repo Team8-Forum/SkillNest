bd0c97d5ac13067b14ea751ed1c1ad4dc80ae18c
Toma Mishev <47216444+TomaMishev@users.noreply.github.com>
2023-11-29 21:12:33 +0200

    Updating Course repo

--- SkillNest/src/main/java/com/example/skillnest/repositories/CourseRepositoryImpl.java
+++ SkillNest/src/main/java/com/example/skillnest/repositories/CourseRepositoryImpl.java
@@ -1,4 +1,62 @@
 package com.example.skillnest.repositories;
 
-public class CourseRepositoryImpl {
+import com.example.skillnest.exceptions.EntityNotFoundException;
+import com.example.skillnest.models.Course;
+import com.example.skillnest.repositories.contracts.CourseRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CourseRepositoryImpl implements CourseRepository {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public CourseRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<Course> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Course> query = session.createQuery("from Course", Course.class);
+            return query.list();
+        }
+    }
+
+    @Override
+    public Course get(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            Course post = session.get(Course.class, id);
+            if (post == null) {
+                throw new EntityNotFoundException("Course", id);
+            }
+            return post;
+        }
+    }
+
+    @Override
+    public Course get(String name) {
+
+    }
+
+    @Override
+    public void create(Course course) {
+
+    }
+
+    @Override
+    public void update(Course course) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
--- SkillNest/src/main/java/com/example/skillnest/repositories/contracts/CourseRepository.java
+++ SkillNest/src/main/java/com/example/skillnest/repositories/contracts/CourseRepository.java
@@ -1,4 +1,19 @@
 package com.example.skillnest.repositories.contracts;
 
+import com.example.skillnest.models.Course;
+
+import java.util.List;
+
 public interface CourseRepository {
+    List<Course> getAll();
+
+    Course get(int id);
+
+    Course get(String name);
+
+    void create(Course course);
+
+    void update(Course course);
+
+    void delete(int id);
 }
