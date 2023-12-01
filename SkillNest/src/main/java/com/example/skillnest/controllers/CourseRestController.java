d4b4e1ebafce5a160fb11aadb9a62eb00d5ff3b2
Toma Mishev <47216444+TomaMishev@users.noreply.github.com>
2023-12-01 19:07:49 +0200

    Uploading partial controller

--- SkillNest/src/main/java/com/example/skillnest/controllers/CourseRestController.java
+++ SkillNest/src/main/java/com/example/skillnest/controllers/CourseRestController.java
@@ -1,4 +1,44 @@
 package com.example.skillnest.controllers;
 
+import com.example.skillnest.exceptions.AuthorizationException;
+import com.example.skillnest.exceptions.EntityNotFoundException;
+import com.example.skillnest.helpers.AuthenticationHelper;
+import com.example.skillnest.models.Course;
+import com.example.skillnest.models.User;
+import com.example.skillnest.models.dtos.CourseDto;
+import com.example.skillnest.services.CourseServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
 public class CourseRestController {
+    private final CourseServiceImpl service;
+    @Autowired
+    public CourseRestController(CourseServiceImpl service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Course> get() {
+      return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Course get(@PathVariable int id) {
+        try {
+            return service.get(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+
 }
