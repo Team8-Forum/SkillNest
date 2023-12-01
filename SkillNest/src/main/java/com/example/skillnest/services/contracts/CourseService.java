01c1a6c7b52cbcd02b1eb74edcca4b731b077bd6
Toma Mishev <47216444+TomaMishev@users.noreply.github.com>
2023-12-01 13:56:00 +0200

    Creating Service layer

--- SkillNest/src/main/java/com/example/skillnest/services/contracts/CourseService.java
+++ SkillNest/src/main/java/com/example/skillnest/services/contracts/CourseService.java
@@ -1,4 +1,19 @@
 package com.example.skillnest.services.contracts;
 
+import com.example.skillnest.models.Course;
+import com.example.skillnest.models.User;
+
+import java.util.List;
+
 public interface CourseService {
+    List<Course> getAll();
+
+    Course get(int id);
+
+    void create(Course course, User user);
+
+    void update(Course course, User user);
+
+    void delete (int id, User user);
+
 }
