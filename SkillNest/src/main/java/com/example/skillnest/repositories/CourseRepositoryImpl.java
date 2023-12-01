a971ef8106bfb4fc8b8e495446d65fe4727916eb
Toma Mishev <47216444+TomaMishev@users.noreply.github.com>
2023-12-01 13:40:30 +0200

    Adding CRUD operations in the Repo

--- SkillNest/src/main/java/com/example/skillnest/repositories/CourseRepositoryImpl.java
+++ SkillNest/src/main/java/com/example/skillnest/repositories/CourseRepositoryImpl.java
@@ -42,21 +42,33 @@ public class CourseRepositoryImpl implements CourseRepository {
 
     @Override
     public Course get(String name) {
-
     }
 
     @Override
     public void create(Course course) {
-
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.persist(course);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void update(Course course) {
-
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.merge(course);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void delete(int id) {
-
+        Course courseToDelete = get(id);
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.remove(courseToDelete);
+            session.getTransaction().commit();
+        }
     }
 }
