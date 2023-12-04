3e442a7272b52cc3116438ef2f4ef0483b714fac
KatyaStepanova <kateryna.stepanova99@gmail.com>
2023-12-04 19:35:54 +0200

    LectureService interface

--- SkillNest/src/main/java/com/example/skillnest/services/contracts/LectureService.java
+++ SkillNest/src/main/java/com/example/skillnest/services/contracts/LectureService.java
@@ -1,4 +1,15 @@
 package com.example.skillnest.services.contracts;
 
+import com.example.skillnest.models.Lecture;
+import com.example.skillnest.models.User;
+import com.example.skillnest.models.dtos.LectureDto;
+
 public interface LectureService {
+    Lecture getById(int id);
+
+    Lecture create(LectureDto lectureDto, User user);
+
+    Lecture update(LectureDto lectureDto, User user, int lectureId);
+
+    void delete(int lectureId, User user);
 }
